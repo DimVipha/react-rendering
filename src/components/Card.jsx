12835038e204fe694670da7f5c205ffd8b9c48a8
vipha <annaella976@gmail.com>
2024-03-09 23:19:23 +0700

    004

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { Card } from 'flowbite-react';
 function Component() {
   return (
     <div className="flex justify-center">
-      <Card className="max-w-sm flex flex-col justify-center w-[95%] ml-5 mb-5 mt-5 mr-5" imgSrc="https://4.imimg.com/data4/GJ/RO/GLADMIN-140939/wireless-computer-250x250.jpg" >
+      <Card className="max-w-sm flex flex-col justify-center w-[95%] ml-5 mb-5 mt-5 mr-5" imgSrc="https://i.pinimg.com/564x/36/14/91/3614913a0a5208cd329b929ca8a005fb.jpg" >
         <h5 className="flex justify-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           PC
         </h5>
@@ -11,7 +11,7 @@ function Component() {
           &#11088;&#11088;&#11088;&#11088;&#11088;
         </p>
         <p className="flex justify-center font-normal text-gray-700 dark:text-gray-400">
-          Best Computer Ever
+          computer for student 
         </p>
       </Card>
     </div>
